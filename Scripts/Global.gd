2333a6bd0b0565b9a84bdb2808f8659a88bb96c5
slost <slowslost@gmail.com>
2023-11-12 01:05:07 +0700

    Add Folders & Global.gd

---
+++ Scripts/Global.gd
@@ -0,0 +1,16 @@
+## สคริปต์สำหรับรวบรวม Global variables
+extends Node
+
+# ถาดสีหลัก
+var main_pallet: Array = [
+	Color.html("#000000"),
+	Color.html("#800080")
+]
+
+# อาเรย์ถาดสี (เผื่อทำระบบเปลี่ยนถาดสี)
+var pallets: Array = [
+	main_pallet
+]
+
+# โหมดแสดงผลความโปร่งใส
+var is_alpha_mode: bool = false
