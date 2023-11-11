edeb57cf574188131943fc1f54286d531718de54
slost <slowslost@gmail.com>
2023-11-12 04:27:18 +0700

    Add Limited Movement

--- Scenes/Character/player.gd
+++ Scenes/Character/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # export
 @export var stats: Node
+@export var inv: Node
 
 # onready
 @onready var speed = stats.base_speed
@@ -10,9 +11,16 @@ extends CharacterBody2D
 # การควบคุม
 func get_input() -> void:
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if !has_node("Inventory/FreeMovement"):
+		if abs(input_direction.x) > abs(input_direction.y):
+			input_direction.y = 0
+		else:
+			input_direction.x = 0
 	velocity = input_direction * speed
 
 
 func _physics_process(_delta) -> void:
 	get_input()
+	var size_scale = stats.size_scale
+	scale = Vector2(size_scale, size_scale)
 	move_and_slide()
--- Scenes/Character/stats.gd
+++ Scenes/Character/stats.gd
@@ -1,4 +1,10 @@
-extends Node2D
+extends Node
 
 ## ความเร็วฐาน
 @export var base_speed = 400
+
+## พลังชีวิตฐาน
+@export var base_hp = 3
+
+## สเกลขนาด
+@export var size_scale = 4
