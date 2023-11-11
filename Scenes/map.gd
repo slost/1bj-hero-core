09cd67f3d92d73f9bf1d07c0adb5ae0265c9be2c
slost <slowslost@gmail.com>
2023-11-12 02:38:17 +0700

    Add draw_background()

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+
+func _draw():
+	draw_background(Global.main_pallet[0])
+	
+
+func draw_background(color: Color) -> void:
+	var viewport_size = get_viewport().get_visible_rect().size
+	var width = viewport_size.x
+	var hieght = viewport_size.y
+	draw_rect(Rect2(0, 0, width, hieght), color)
+	print_debug(color)
