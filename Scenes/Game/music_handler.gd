665e5af9af252370e165134ca1f31e6d8dc0104f
slost <slowslost@gmail.com>
2023-11-13 03:19:03 +0700

    Update MusicHandler

--- Scenes/Game/music_handler.gd
+++ Scenes/Game/music_handler.gd
@@ -1,40 +1,44 @@
 extends Node
 
-@export var tempo: float = 140.0
+@export var tempo: float = 90
 
 var time: float = 0.0
 var beats: int = 1
 var bars: int = 1
-var bar_timer = 0
-var beats_timer = 0
+var bar_timer: float = 0.0
+var beats_timer: float = 0.0
+
 var is_played = false
 
 func _process(_delta) -> void:
+
 	Global.tempo = tempo
+	
+	var seconds_per_bar: float = 60 / tempo
 
-	time += 1
-	bar_timer += 1
-	beats_timer += 1
-
-	if beats_timer == tempo:
-		beats += 1
-		beats_timer = 0
-		
-	if bar_timer == tempo / 4:
+	time += _delta
+	bar_timer += _delta
+	
+	if bar_timer >= seconds_per_bar:
+		is_played = false
 		bars += 1
-		bar_timer = 0
+		bar_timer -= seconds_per_bar
 		
 	if bars > 4:
-		bars = 1	
+		bars = 1
+		beats += 1
 	
 	if Global.is_debugging:
-		$Debugger.text = "Time: " + str(time) + " (" + str( round(time / 60) ) \
-		+ "s)"+ "\nBeats: "+ str(beats) + "." + str(bars) 
-	
+		$Debugger.text =  "Tempo: %s bpm\n" % tempo  + \
+		"Time: " + str(round(time)) + "s" + "\nBeats: "+ str(beats) + "." + str(bars) 
+		
 	if !is_played:
 		play_sound(1, $Kick)
+		play_sound(2, $Snare)
+		play_sound(3, $Kick)
+		play_sound(4, $Snare)
 		is_played = true
-
+	
 
 func play_sound(_bar, _sound) -> void:
 	if bars == _bar:
