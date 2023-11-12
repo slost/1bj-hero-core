cdff0502a668f8effedd8f595a2703ff5683ce64
slost <slowslost@gmail.com>
2023-11-13 03:56:49 +0700

    Add debug_tempo

--- Scenes/Game/debug_tool.gd
+++ Scenes/Game/debug_tool.gd
@@ -1,4 +1,13 @@
 extends Node
+
+@export var musicH: Node
+
 func _input(_event):
 	if Input.is_action_pressed("debug_restart"):
 		get_tree().reload_current_scene()
+	if Input.is_action_pressed("debug_tempo_increase"):
+		musicH.tempo += 1
+	if Input.is_action_pressed("debug_tempo_decrease"):
+		musicH.tempo -= 1
+		
+		
