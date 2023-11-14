08c56fa843d3f2329461a38ca4c736c51cb31ec8
slost <slowslost@gmail.com>
2023-11-14 14:53:56 +0700

    Add Hud

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -3,3 +3,9 @@ extends Control
 func _process(_delta):
 	$SubViewportContainer.material.set_shader_parameter("pal_swap_1", Global.palette[0])
 	$SubViewportContainer.material.set_shader_parameter("pal_swap_2", Global.palette[1])
+	set_hud_text()
+	
+	
+func set_hud_text() -> void:
+	$RichTextLabel.clear()
+	$RichTextLabel.append_text( "TEMPO: %s" % Global.tempo )
