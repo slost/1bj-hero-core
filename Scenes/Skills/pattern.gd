f46a9cfb0b1afae67e7a384e23e6da5d7714f759
slost <slowslost@gmail.com>
2023-11-14 22:51:16 +0700

    Improve Pattern

--- Scenes/Skills/pattern.gd
+++ Scenes/Skills/pattern.gd
@@ -2,20 +2,22 @@ extends TileMap
 
 var layer: int = 0
 var type = "type"
-var max_range: int = 10
+
+
+func _init():
+	visible = false
+	get_pattern_tiles()
 
 
 func get_pattern_tiles() -> Array:
 	var tile_map: Array = []
-	for x in range(-max_range, max_range):
-		for y in range(-max_range, max_range):
-			var tile: Array = []
-			var tile_data = get_cell_tile_data(0, Vector2(x, y))
-			if tile_data:
-				tile.append(Vector2(x, y))
-				var _type = tile_data.get_custom_data(type)
-				if _type == "se":
-					print("go se")
-					tile.append(_type)
-				tile_map.append(tile)
+	for i in get_used_cells(layer):
+		var tile: Array = []
+		tile.append(Vector2(i.x, i.y))
+		var tile_data = get_cell_tile_data(0, i)
+		var _type = tile_data.get_custom_data(type)
+		if _type == "se":
+			print("go se")
+			tile.append(_type)
+		tile_map.append(tile)
 	return tile_map
--- Scripts/skill.gd
+++ Scripts/skill.gd
@@ -3,13 +3,9 @@ extends Node
 var proj_path = "res://Scenes/Skills/projectile.tscn"
 var bars: int = 1
 
+@export var pattern: Node
+
 @onready var player: Node = $"../.."
-
-var pattern
-
-func _ready():
-	pattern = $Around
-	pattern.visible = false
 
 
 func _process(_delta) -> void:
@@ -21,8 +17,8 @@ func _process(_delta) -> void:
 	
 
 # ใช้สปอนสกิล arg0 = id skill, arg1 = paremeters (จะใส่ไม่ใส่ก็ได้)
-func spawn_projectile(_skill: String, _params = null) -> void:
-	var skill = load(proj_path).instantiate()
+func spawn_projectile(_projectile: String, _params = null) -> void:
+	var proj = load(proj_path).instantiate()
 	# if _params != null:
 	var spawn_loc = _params.get("spawn_loc", Vector2(0, 0)) # ตำแหน่งสปอน
 	var spawn_dist = _params.get("spawn_dist", 0) # ระยะห่างจากตำแหน่งสปอน
@@ -30,15 +26,15 @@ func spawn_projectile(_skill: String, _params = null) -> void:
 	var skill_dir
 	var spawn_position = player.global_position \
 	+ ( (spawn_loc * Global.tile_size * player.scale) ) * (spawn_dist + 1)
-	skill.scale = proj_scale * Global.scale
-	skill.global_position = spawn_position
-	$"..".add_child(skill)
+	proj.scale = proj_scale * Global.scale
+	proj.global_position = spawn_position
+	$"..".add_child(proj)
 	
 
 func spawn_projectile_pattern(_skill: String, _pattern: String, _params = null) -> void:
 	var spawn_dist = _params.get("spawn_dist", 0)
 	var pattern_tiles = pattern.get_pattern_tiles()
-	print(pattern_tiles)
+	# print_debug(pattern_tiles)
 	for tile in pattern_tiles:
 		spawn_projectile("kick", {spawn_loc = tile[0], spawn_dist = spawn_dist})
 		
