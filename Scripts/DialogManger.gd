36a064ae7f68b8d1fb7864ceb849beb2442c1763
jelato-js <89293259+jelato-js@users.noreply.github.com>
2023-11-15 00:38:13 +0700

    Update Dialog

--- Scripts/DialogManger.gd
+++ Scripts/DialogManger.gd
@@ -11,19 +11,35 @@ var text_box_position: Vector2
 var is_dialog_active = false
 var can_advance_line = false
 
-func start_dialog(posistion: Vector2, lines: Array[String]):
+var is_fix_massagebox = false
+var fix_posistion = Vector2(384, 704)
+
+func start_dialog(posistion: Vector2, lines: Array[String], fix_massagebox:bool = false):
 	if is_dialog_active:
 		return
 	dialog_lines = lines
-	text_box_position = posistion
+	if fix_massagebox:
+		is_fix_massagebox = true
+		text_box_position = fix_posistion
+	else:
+		text_box_position = posistion
+	print_debug( text_box_position)
 	_show_text_box()
 	is_dialog_active = true
 
 func _show_text_box():
 	text_box = text_box_scene.instantiate()
 	text_box.finsished_displaying.connect(_on_text_box_finished_displaying)
-	get_tree().root.add_child(text_box)
+	#get_tree().root.get_node('/root/Game/SubViewportContainer/SubViewport').add_child(text_box)
+	#get_tree().root.add_child(text_box)
 	text_box.global_position = text_box_position
+	if is_fix_massagebox:
+		text_box.set_fix_massagebox()
+		get_tree().root.get_node('/root/Game/DialogCanvasLayer').add_child(text_box)
+		#text_box.set_anchors_preset(Control.PRESET_CENTER_BOTTOM, true)
+	else:
+		get_tree().root.add_child(text_box)
+	
 	text_box.display_text(dialog_lines[current_line_index])
 	can_advance_line = false
 
@@ -37,7 +53,6 @@ func _unhandled_input(event):
 		can_advance_line
 	):
 		text_box.queue_free()
-		print_debug("text_box.queue_free()")
 		current_line_index += 1
 		if current_line_index >= dialog_lines.size():
 			is_dialog_active = false
