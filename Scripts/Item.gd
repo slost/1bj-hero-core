a1f33cb022053c34b62032b762b460e33766fd01
slost <slowslost@gmail.com>
2023-11-14 16:09:10 +0700

    Update Item.gd

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -9,8 +9,8 @@ var bars: int = 1
 func _process(_delta) -> void:
 	if Global.bars >= bars:
 		spawn_skill("kick", {skill_scale = 4})
-		#if Global.bars % 2 == 0:
-			#release_skill("snare")
+		if Global.bars % 2 == 0:
+			spawn_skill_pattern("kick", "around caster", {spawn_dist = 1})
 		bars += 1
 	
 
@@ -20,15 +20,31 @@ func spawn_skill(_skill: String, _params = null) -> void:
 	# if _params != null:
 	var spawn_loc = _params.get("spawn_loc", "mid")
 	var spawn_dist = _params.get("spawn_dist", 0)
-	var skill_scale = _params.get("skill_scale", 8)
-	skill.global_position = player.global_position \
-		+ get_spawn_location(spawn_loc, spawn_dist) * player.scale
-	skill.scale = player.scale * (skill_scale * Global.scale)
+	var skill_scale = _params.get("skill_scale", 1)
+	var skill_dir
+	var spawn_position = player.global_position + \
+		( get_spawn_location(spawn_loc)  * (spawn_dist + 1) )
+	skill.scale = (skill_scale * Global.scale)
+	skill.global_position = spawn_position
 	$"..".add_child(skill)
 	
 
+func spawn_skill_pattern(_skill: String, _pattern: String, _params = null) -> void:
+	var spawn_dist = _params.get("spawn_dist", 0)
+	match _pattern:
+		"around caster":
+			spawn_skill(_skill, {spawn_loc = "n", spawn_dist = spawn_dist})
+			spawn_skill(_skill, {spawn_loc = "nw"})
+			spawn_skill(_skill, {spawn_loc = "e"})
+			spawn_skill(_skill, {spawn_loc = "w"})
+			spawn_skill(_skill, {spawn_loc = "s"})
+			spawn_skill(_skill, {spawn_loc = "ne"})
+			spawn_skill(_skill, {spawn_loc = "sw"})
+			spawn_skill(_skill, {spawn_loc = "se"})
+
+
 # รีเทิร์นตำแหน่ง arg0 = ตำแหน่ง, arg1 = ระยะห่างจากตำแหน่งนั้น
-func get_spawn_location(_location: String, _distance: int) -> Vector2:
+func get_spawn_location(_location: String) -> Vector2:
 	var loc_vec: Vector2
 	match _location:
 		"nw":
@@ -49,6 +65,6 @@ func get_spawn_location(_location: String, _distance: int) -> Vector2:
 			loc_vec = Vector2.DOWN
 		"se":
 			loc_vec = Vector2.ONE 
-	return loc_vec * Global.tile_size * player.scale * (_distance + 1)
+	return loc_vec * Global.tile_size * player.scale
 		
 
