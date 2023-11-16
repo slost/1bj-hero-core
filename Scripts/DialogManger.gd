b1dc2c4be42c5b077383c1d78b81f1c02af4bef4
jelato-js <89293259+jelato-js@users.noreply.github.com>
2023-11-17 01:53:38 +0700

    Update DialogCanvas

--- Scripts/DialogManger.gd
+++ Scripts/DialogManger.gd
@@ -23,28 +23,39 @@ func start_dialog(position: Vector2, lines: Array[String], fix_massagebox:bool =
 	dialog_lines = lines
 	if fix_massagebox:
 		is_fix_massagebox = true
-		var fix_box = Global.map.get_node("DialogCanvasLayer/PositionTextBox")
+		var fix_box = Global.dialogCanvas.get_node("TextBox - DEBUG")#Global.map.get_node("DialogCanvasLayer/PositionTextBox")
 		text_box_position = fix_box.position
-		print_debug(text_box_position)
 	else:
 		text_box_position = position
 	_show_text_box()
 	is_dialog_active = true
 
 func _show_text_box():
-	print_debug("_show_text_box")
 	text_box = text_box_scene.instantiate()
 	text_box.finsished_displaying.connect(_on_text_box_finished_displaying)
 	#get_tree().root.get_node('/root/Game/SubViewportContainer/SubViewport').add_child(text_box)
 	#get_tree().root.add_child(text_box)
-	#text_box.global_position = text_box_position
+	text_box.global_position = text_box_position
 	if is_fix_massagebox:
 		text_box.set_fix_massagebox()
-		Global.map.get_node('DialogCanvasLayer').add_child(text_box)
-		text_box.set_anchors_preset(Control.PRESET_CENTER_BOTTOM, true)
-		text_box.set_position(text_box_position)
+		#Global.map.get_node('DialogCanvasLayer').add_child(text_box)
+		Global.dialogCanvas.add_child(text_box)
+		#text_box.set_anchors_preset(Control.PRESET_CENTER, true)
+		
+		var fix_box = Global.dialogCanvas.get_node("PositionTextBox")#Global.map.get_node("DialogCanvasLayer/PositionTextBox")
+		text_box_position = fix_box.position
+		
+		print_debug(Global.dialogCanvas.get_node("PositionTextBox").position)
+		print_debug(Global.dialogCanvas.get_node("TextBox - DEBUG").position)
+		
+		#text_box.set_position(text_box_position)
+		#text_box.set_anchors_preset(Control.PRESET_CENTER)
+		#text_box.position.x = text_box_position.x + text_box_position.x / 2
+		#text_box.position.y = text_box_position.y + text_box_position.y / 2
 		text_box.pivot_offset.x = text_box.size.x / 2
 		text_box.pivot_offset.y = text_box.size.y / 2
+		#text_box.set_anchors_preset(Control.PRESET_CENTER, true)
+		
 	else:
 		get_tree().root.add_child(text_box)
 	text_box.global_position = text_box_position
