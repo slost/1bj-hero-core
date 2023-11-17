acb84d5a4e760994b4223c2edad45e5396d36bb2
slost <slowslost@gmail.com>
2023-11-17 18:00:05 +0700

    Improve Pattern tool: Auto set layer name

--- Scenes/Skills/pattern.gd
+++ Scenes/Skills/pattern.gd
@@ -2,9 +2,10 @@
 extends TileMap
 class_name SkillPattern
 
-func _init():
+func _ready():
 	# get_pattern_tiles()
 	set("tile_set", load("res://Scenes/Skills/pattern_tile.tres"))
+	set("layer_0/name", "direction")
 	add_child(load("res://Database/Patterns/dummy_caster.tscn").instantiate())
 
 
