0345610dc0c7bb552a1f3b3d9c8239a50baf6021
slost <slowslost@gmail.com>
2023-12-01 20:58:05 +0700

    Update interface: Monster tempo & bars

--- Scenes/Game/Interface.gd
+++ Scenes/Game/Interface.gd
@@ -8,5 +8,8 @@ func _process(_delta):
 	var player_tempo = "[center]TEMPO: " + str(player.music.tempo)
 	if Global.player.music:
 		playerTempoLabel.text = player_tempo + "\n" + Lib.get_bars_string(player.bars) 
-
+	var boss = Global.boss
+	var boss_tempo = "[center]TEMPO: " + str(boss.music.tempo)
+	if Global.boss.music:
+		monsterTempoLabel.text = boss_tempo + "\n" + Lib.get_bars_string(boss.bars)
 
