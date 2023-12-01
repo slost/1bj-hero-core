43db34c299509c89a25b6803bd8b99fd4cb6eb5c
slost <slowslost@gmail.com>
2023-12-01 22:26:46 +0700

    Add Interface sprite & Play button

--- Scenes/Character/stats.gd
+++ Scenes/Character/stats.gd
@@ -1,10 +1,12 @@
 extends Node
 
 ## ความเร็วฐาน
-@export var base_speed = 400
+@export var base_speed = 50
 
 ## พลังชีวิตฐาน
 @export var base_hp = 3
 
 ## สเกลขนาด
 @export var size_scale = 4
+
+@export var zoom = 0.25
